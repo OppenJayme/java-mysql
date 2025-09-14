b0b6b7ae1ae1651c8024778def42f70113afdf4a
OppenJayme <jaymekyerivan@gmail.com>
2025-09-15 00:08:38 +0800

    add: reusable

--- src/mysqldemo.java
+++ src/mysqldemo.java
@@ -4,27 +4,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class mysqldemo {
-    public static void main(String args[]) throws Exception{
+
+
+    public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         Properties props = new Properties();
         props.load(new FileInputStream(".env.local"));
 
         String db_name = props.getProperty("DB_NAME");
-        String url ="jdbc:mysql://localhost:3306/" + db_name;
+        String url = "jdbc:mysql://localhost:3306/" + db_name + "?useSSL=false&serverTimezone=UTC";
         String user = props.getProperty("DB_USER");
         String password = props.getProperty("DB_PASS");
 
-        try (Connection connection = DriverManager.getConnection(url,user,password);){
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    // For testing only
+    public static void main(String args[]) {
+        try (Connection connection = getConnection()) {
             System.out.println("connected!");
         } catch (SQLException e) {
             System.out.println("sql error: " + e.getMessage());
             System.out.println("SQLState: " + e.getSQLState());
             System.out.println("ErrorCode: " + e.getErrorCode());
-            e.printStackTrace(); // helpful while debugging
-            System.out.println("sql error: " + e.getMessage()); // show real sql error "WHY IT FAILED"
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
